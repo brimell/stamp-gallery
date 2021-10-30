f8a5f7f73c9bb03d6b31c6e5224bf6410e10ed74
Raaydon <billjrimell@gmail.com>
2021-10-30 12:28:07 +0100

    added stamp and flag images

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -11,16 +11,6 @@ import {
   UncontrolledDropdown
 } from "reactstrap";
 import Widget from "../../components/Widget/Widget.js";
-import ApexActivityChart from "./components/ActivityChart.js";
-
-import meal1 from "../../assets/dashboard/meal-1.svg";
-import meal2 from "../../assets/dashboard/meal-2.svg";
-import meal3 from "../../assets/dashboard/meal-3.svg";
-import upgradeImage from "../../assets/dashboard/upgradeImage.svg";
-import heartRed from "../../assets/dashboard/heartRed.svg";
-import heartTeal from "../../assets/dashboard/heartTeal.svg";
-import heartViolet from "../../assets/dashboard/heartViolet.svg";
-import heartYellow from "../../assets/dashboard/heartYellow.svg";
 import gymIcon from "../../assets/dashboard/gymIcon.svg";
 import therapyIcon from "../../assets/dashboard/therapyIcon.svg";
 import user from "../../assets/user.svg";
@@ -29,14 +19,18 @@ import statsPie from "../../assets/dashboard/statsPie.svg";
 import s from "./Dashboard.module.scss";
 
 const Dashboard = () => {
-  const [checkboxes, setCheckboxes] = useState([true, false])
+  // const [checkboxes, setCheckboxes] = useState([true, false])
 
-  const toggleCheckbox = (id) => {
-    setCheckboxes(checkboxes => checkboxes
-      .map((checkbox, index) => index === id ? !checkbox : checkbox ))
-  }
+  // const toggleCheckbox = (id) => {
+  //   setCheckboxes(checkboxes => checkboxes
+  //     .map((checkbox, index) => index === id ? !checkbox : checkbox ))
+  // }
 
-  const meals = [meal1, meal2, meal3];
+  const books = [['A1','500'],['B1','200'],['C1','300']];
+  const totalStamps = 1000
+  const flags = [
+    []
+  ]
 
   return (
     <div>
@@ -44,51 +38,40 @@ const Dashboard = () => {
         <Col className="pr-grid-col" xs={12} lg={8}>
           <Row className="gutter mb-4">
             <Col className="mb-4 mb-md-0" xs={12} md={6}>
-              <Widget className="">
-                <div className="d-flex justify-content-between widget-p-md">
+            <Widget className="widget-p-md">
+                <div className="d-flex justify-content-between">
                   <div className="headline-3 d-flex align-items-center">Books</div>
                 </div>
-              </Widget>
-            </Col>
-            <Col xs={12} md={6}>
-              <Widget className="widget-p-md">
-                <div className="d-flex justify-content-between">
-                  <div className="headline-3 d-flex align-items-center">Globe</div>
-                </div>
-                {meals.map((meal) =>
+                {books.map((book) =>
                   <div key={uuidv4()} className={`mt-4 ${s.widgetBlock}`}>
                     <div className={s.widgetBody}>
                       <div className="d-flex">
-                        <img className="img-fluid mr-2" src={meal} alt="..." />
+                        <icon className="eva eva-book-outline" alt="..." />
                         <div className="d-flex flex-column">
-                          <p className="body-2">Salmon salad</p>
-                          <p className="body-3 muted">300 g</p>
+                          <p className="body-2">{book[0]}</p>
+                          {/* <p className="body-3 muted">{book[1]}</p> */}
                         </div>
                       </div>
-                      <div className="body-3 muted">
-                        175 cal
-                      </div>
+                      <div className="body-3 muted">{book[1]} stamps</div>
                     </div>
                   </div>
                 )}
               </Widget>
             </Col>
+            <Col xs={12} md={6}>
+            <Widget className="">
+                <div className="d-flex justify-content-between widget-p-md">
+                  <div className="headline-3 d-flex align-items-center">Globe</div>
+                </div>
+              </Widget>
+              
+            </Col>
           </Row>
           <Row className="gutter mb-4">
             <Col xs={12}>
               <Widget className="widget-p-none">
-                <div className="d-flex flex-wrap align-items-center justify-content-center">
-                  <div className="d-flex flex-column align-items-center col-12 col-xl-6 p-sm-4">
-                    <p className="headline-1">Upgrade your plan</p>
-                    <p className="body-3">So how did the classical Latin become so </p>
-                    <div className="d-flex justify-content-between my-4">
-                      <Button className="rounded-pill mr-3" color="primary">Go Premium</Button>
-                      <Button className="rounded-pill body-3" outline color="dark">Try for free</Button>
-                    </div>
-                  </div>
-                  <div className="d-flex justify-content-center col-12 col-xl-6">
-                    <img className="p-1 img-fluid" src={upgradeImage} alt="..." />
-                  </div>
+              <div className="d-flex justify-content-between widget-p-md">
+                  <div className="headline-3 d-flex align-items-center">Debug Console</div>
                 </div>
               </Widget>
             </Col>
@@ -98,10 +81,10 @@ const Dashboard = () => {
               <Widget className="widget-p-sm">
                 <div className={s.smallWidget}>
                   <div className="d-flex mb-4">
-                    <img className="py-1 mr-2 img-fluid" src={heartRed} alt="..." />
+                    {/* <img className="py-1 mr-2 img-fluid" src={heartRed} alt="..." /> */}
                     <div className="d-flex flex-column">
-                      <p className="headline-3">Text</p>
-                      <p className="body-2">Num<span className="body-3 muted">/ ber</span></p>
+                      <p className="headline-3">Total Stamps</p>
+                      <p className="body-2">- <span className="body-3 muted">{totalStamps}</span></p>
                     </div>
                   </div>
                   <div>
@@ -114,10 +97,10 @@ const Dashboard = () => {
               <Widget className="widget-p-sm">
                 <div className={s.smallWidget}>
                   <div className="d-flex mb-4">
-                    <img className="py-1 mr-2 img-fluid" src={heartYellow} alt="..." />
+                    {/* <img className="py-1 mr-2 img-fluid" src={heartYellow} alt="..." /> */}
                     <div className="d-flex flex-column">
-                      <p className="headline-3">Text</p>
-                      <p className="body-2">Num<span className="body-3 muted">/ ber</span></p>
+                      <p className="headline-3">Unique Stamps</p>
+                      <p className="body-2">- <span className="body-3 muted">300</span></p>
                     </div>
                   </div>
                   <div>
@@ -130,10 +113,10 @@ const Dashboard = () => {
               <Widget className="widget-p-sm">
                 <div className={s.smallWidget}>
                   <div className="d-flex mb-4">
-                    <img className="py-1 mr-2 img-fluid" src={heartTeal} alt="..." />
+                    {/* <img className="py-1 mr-2 img-fluid" src={heartTeal} alt="..." /> */}
                     <div className="d-flex flex-column">
                       <p className="headline-3">Text</p>
-                      <p className="body-2">Num<span className="body-3 muted">/ ber</span></p>
+                      <p className="body-2">- <span className="body-3 muted">300</span></p>
                     </div>
                   </div>
                   <div>
@@ -146,10 +129,10 @@ const Dashboard = () => {
               <Widget className="widget-p-sm">
                 <div className={s.smallWidget}>
                   <div className="d-flex mb-4">
-                    <img className="py-1 mr-2 img-fluid" src={heartViolet} alt="..." />
+                    {/* <img className="py-1 mr-2 img-fluid" src={heartViolet} alt="..." /> */}
                     <div className="d-flex flex-column">
                       <p className="headline-3">Text</p>
-                      <p className="body-2">Num<span className="body-3 muted">/ ber</span></p>
+                      <p className="body-2">- <span className="body-3 muted">300</span></p>
                     </div>
                   </div>
                   <div>
@@ -165,110 +148,10 @@ const Dashboard = () => {
             <div className="d-flex">
               <img className={s.image} src={user} alt="..." />
               <div className={s.userInfo}>
-                <p className="headline-3">Christina Karey</p>
-                <p className="body-3 muted">Brasil</p>
+                <p className="headline-3">name</p>
+                <p className="body-3 muted">info</p>
               </div>
             </div>
-            <div className={s.userParams}>
-              <div className="d-flex flex-column">
-                <p className="headline-3">63 kg</p>
-                <p className="body-3 muted">Weight</p>
-              </div>
-              <div className="d-flex flex-column">
-                <p className="headline-3">175 sm</p>
-                <p className="body-3 muted">Height</p>
-              </div>
-              <div className="d-flex flex-column">
-                <p className="headline-3">28 y.</p>
-                <p className="body-3 muted">Age</p>
-              </div>
-            </div>
-            <div className={s.goals}>
-              <div className={s.goalsTitle}>
-                <p className="headline-3">Your Goals</p>
-                <UncontrolledDropdown>
-                  <DropdownToggle caret>
-                    &nbsp; Weekly &nbsp;
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem>Daily</DropdownItem>
-                    <DropdownItem>Weekly</DropdownItem>
-                    <DropdownItem>Monthly</DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </div>
-              <div className="d-flex flex-column mt-3">
-                <div className={s.activity}>
-                  <p className="body-2">Sleep</p>
-                  <p className="body-2">92<span className="body-3 muted"> / 160</span></p>
-                </div>
-                <Progress color="secondary-red" className="progress-xs" value={60} />
-              </div>
-              <div className="d-flex flex-column mt-3">
-                <div className={s.activity}>
-                  <p className="body-2">Sport</p>
-                  <p className="body-2">40<span className="body-3 muted"> / 50</span></p>
-                </div>
-                <Progress color="secondary-yellow" className="progress-xs" value={80} />
-              </div>
-              <div className="d-flex flex-column mt-3">
-                <div className={s.activity}>
-                  <p className="body-2">Water</p>
-                  <p className="body-2">25<span className="body-3 muted"> / 40</span></p>
-                </div>
-                <Progress color="secondary-cyan" className="progress-xs" value={40} />
-              </div>
-            </div>
-            <p className="headline-3">Appointments</p>
-            <div className={`mt-3 ${s.widgetBlock}`}>
-              <div className={s.widgetBody}>
-                <div className="d-flex">
-                  <img className="img-fluid mr-2" src={gymIcon} alt="..." />
-                  <div className="d-flex flex-column">
-                    <p className="body-2">02.11 , 12:00 - 13:00</p>
-                    <p className="body-3 muted">Yoga, Airplace Gym</p>
-                  </div>
-                </div>
-                <div className="checkbox checkbox-primary">
-                  <input
-                    id="checkbox0"
-                    type="checkbox"
-                    className="styled"
-                    checked={checkboxes[0]}
-                    onChange={() => toggleCheckbox(0)}
-                  />
-                  <label htmlFor="checkbox0" />
-                </div>
-              </div>
-            </div>
-            <div className={`mt-3 ${s.widgetBlock}`}>
-              <div className={s.widgetBody}>
-                <div className="d-flex">
-                  <img className="img-fluid mr-2" src={therapyIcon} alt="..." />
-                  <div className="d-flex flex-column">
-                    <p className="body-2">03.11 , 16:00 - 17:30</p>
-                    <p className="body-3 muted">Therapy</p>
-                  </div>
-                </div>
-                <div className="checkbox checkbox-primary">
-                  <input
-                    id="checkbox1"
-                    type="checkbox"
-                    className="styled"
-                    checked={checkboxes[1]}
-                    onChange={() => toggleCheckbox(1)}
-                  />
-                  <label htmlFor="checkbox1" />
-                </div>
-              </div>
-            </div>
-            <a className={`btn-secondary-red ${s.statsBtn}`} href="#top" role="button">
-              <img className={s.pieImg}  src={statsPie} alt="..." />
-              <div>
-                <p className="headline-2">STATISTIC</p>
-                <p className="body-3">Download your activity</p>
-              </div>
-            </a>
           </Widget>
         </Col>
       </Row>
